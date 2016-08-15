0a3e40db21824ffe23320701716e0d87434085fd
Your Name <you@example.com>
2016-08-15 19:13:11 +0800

    初始化

---
+++ FinestFood/FinestFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinestFood
+//
+//  Created by qianfeng on 16/8/15.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
